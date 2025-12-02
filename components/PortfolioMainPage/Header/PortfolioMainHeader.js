b35e567186ceca5b062e518071049e5a005ef6a0
Elisa Linnemannstoens <elisalin2001@gmail.com>
2025-12-03 00:10:26 +0100

    created Header function

---
+++ components/PortfolioMainPage/Header/PortfolioMainHeader.js
@@ -0,0 +1,10 @@
+
+export default function Header(){
+    return(
+        <header>
+            <h1>Elisa Linnemannstöns</h1>
+            <p>Junior Frontend Developer</p>
+            </header>
+        
+    )
+}
